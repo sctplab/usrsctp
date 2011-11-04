495a4014928316508382f161794701c648e3a0bb
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2011-11-04 07:59:04 +0000

    Out go opt_inet.h and opt_inet6.h.

--- trunk/KERN/usrsctp/usrsctplib/netinet/sctp_os_userspace.h
+++ trunk/KERN/usrsctp/usrsctplib/netinet/sctp_os_userspace.h
@@ -41,8 +41,6 @@
 #include <sys/cdefs.h> /* needed? added from old __FreeBSD__ */
 #include <stdio.h>
 #include <string.h>
-#include "opt_inet6.h"
-#include "opt_inet.h"
 #include "opt_sctp.h"
 /* #include <sys/param.h>  in FreeBSD defines MSIZE */
 /* #include <sys/ktr.h> */
