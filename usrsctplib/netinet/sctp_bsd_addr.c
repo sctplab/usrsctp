94aa0ecfb5d039c0bca000e5b975282bffab61a4
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2012-03-09 13:20:09 +0000

    Sync with main sources.

--- usrsctplib/netinet/sctp_bsd_addr.c
+++ usrsctplib/netinet/sctp_bsd_addr.c
@@ -34,7 +34,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_bsd_addr.c 229621 2012-01-05 19:00:36Z jhb $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_bsd_addr.c 232724 2012-03-09 13:15:40Z tuexen $");
 #endif
 
 #include <netinet/sctp_os.h>
@@ -296,6 +296,7 @@ sctp_is_desired_interface_type(struct ifaddr *ifa)
 	case IFT_SLIP:
 	case IFT_GIF:
 	case IFT_L2VLAN:
+	case IFT_STF:
 #if !defined(__APPLE__)
 	case IFT_IP:
 	case IFT_IPOVERCDLC:
