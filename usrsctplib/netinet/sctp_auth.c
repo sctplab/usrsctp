673b16a1099edd7bb4ceb8878d81c1bcc3f0c040
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2012-04-27 14:03:15 +0000

    Sync with main sources.

--- usrsctplib/netinet/sctp_auth.c
+++ usrsctplib/netinet/sctp_auth.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_auth.c 228907 2011-12-27 10:16:24Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_auth.c 234699 2012-04-26 11:07:15Z tuexen $");
 #endif
 
 #include <netinet/sctp_os.h>
