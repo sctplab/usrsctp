c1cdb9fb40c53c276d8dd9850d126a33a113f3af
tuexen <tuexen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2012-06-24 23:24:06 +0000

    Sync with main sources.

--- trunk/KERN/usrsctp/usrsctplib/netinet/sctp_output.c
+++ trunk/KERN/usrsctp/usrsctplib/netinet/sctp_output.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_output.c 237540 2012-06-24 21:25:54Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_output.c 237542 2012-06-24 23:12:24Z tuexen $");
 #endif
 
 #include <netinet/sctp_os.h>
@@ -11647,19 +11647,15 @@ sctp_send_ecn_echo(struct sctp_tcb *stcb, struct sctp_nets *net,
 
 void
 sctp_send_packet_dropped(struct sctp_tcb *stcb, struct sctp_nets *net,
-    struct mbuf *m, int iphlen, int bad_crc)
+    struct mbuf *m, int len, int iphlen, int bad_crc)
 {
 	struct sctp_association *asoc;
 	struct sctp_pktdrop_chunk *drp;
 	struct sctp_tmit_chunk *chk;
 	uint8_t *datap;
-	int len;
 	int was_trunc = 0;
 	struct ip *iph;
-#ifdef INET6
-	struct ip6_hdr *ip6h;
-#endif
-	int fullsz = 0, extra = 0;
+	int fullsz = 0;
 	long spc;
 	int offset;
 	struct sctp_chunkhdr *ch, chunk_buf;
@@ -11689,35 +11685,8 @@ sctp_send_packet_dropped(struct sctp_tcb *stcb, struct sctp_nets *net,
 		sctp_free_a_chunk(stcb, chk, SCTP_SO_NOT_LOCKED);
 		return;
 	}
-	switch (iph->ip_v) {
-#ifdef INET
-	case IPVERSION:
-		/* IPv4 */
-#if defined(__FreeBSD__) || defined(__APPLE__)
-		len = chk->send_size = iph->ip_len;
-#elif defined(__Userspace__)
-#if defined(__Userspace_os_FreeBSD) || defined(__Userspace_os_Darwin)
-                len = chk->send_size = SCTP_GET_IPV4_LENGTH(iph);
-#else
-                len = chk->send_size = (SCTP_GET_IPV4_LENGTH(iph) - iphlen);
-                extra = iphlen;
-#endif
-#else
-		len = chk->send_size = (iph->ip_len - iphlen);
-		extra = iphlen;
-#endif
-		break;
-#endif
-#ifdef INET6
-	case IPV6_VERSION >> 4:
-		/* IPv6 */
-		ip6h = mtod(m, struct ip6_hdr *);
-		len = chk->send_size = htons(ip6h->ip6_plen);
-		break;
-#endif
-	default:
-		return;
-	}
+	len -= iphlen;
+	chk->send_size = len;
         /* Validate that we do not have an ABORT in here. */
 	offset = iphlen + sizeof(struct sctphdr);
 	ch = (struct sctp_chunkhdr *)sctp_m_getptr(m, offset,
@@ -11753,8 +11722,8 @@ sctp_send_packet_dropped(struct sctp_tcb *stcb, struct sctp_nets *net,
 		/* only send 1 mtu worth, trim off the
 		 * excess on the end.
 		 */
-		fullsz = len - extra;
-		len = min(stcb->asoc.smallest_mtu,MCLBYTES) - SCTP_MAX_OVERHEAD;
+		fullsz = len;
+		len = min(stcb->asoc.smallest_mtu, MCLBYTES) - SCTP_MAX_OVERHEAD;
 		was_trunc = 1;
 	}
 	chk->asoc = &stcb->asoc;
