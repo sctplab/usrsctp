39f7855c5054df6821eee9198f9d65e210d40d06
t00fcxen <t00fcxen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2012-09-04 20:02:42 +0000

    More cleanups...

--- usrsctplib/user_ip6_var.h
+++ usrsctplib/user_ip6_var.h
@@ -84,34 +84,18 @@ struct ip6_hdr {
 #define IPV6_VERSION		0x60
 #endif
 
+#if defined(__Userspace_os_Windows)
+#define s6_addr16 u.Word
+#endif
 #if !defined(__Userspace_os_Windows)
 #if !defined(__Userspace_os_Linux)
 #define s6_addr8  __u6_addr.__u6_addr8
 #define s6_addr16 __u6_addr.__u6_addr16
 #define s6_addr32 __u6_addr.__u6_addr32
-#else
-#define IPPROTO_DONE 257
-struct in6_pktinfo {
-	struct in6_addr ipi6_addr;
-	int ipi6_ifindex;
-};
 #endif
-#else
-#define IPPROTO_DONE 257
-#define s6_addr16 u.Word
 #endif
 
 #if !defined(__Userspace_os_FreeBSD)
-struct ip6_moptions {
-	struct	ifnet *im6o_multicast_ifp; /* ifp for outgoing multicasts */
-	u_char	im6o_multicast_hlim;	/* hoplimit for outgoing multicasts */
-	u_char	im6o_multicast_loop;	/* 1 >= hear sends if a member */
-	u_short	im6o_num_memberships;	/* no. memberships this socket */
-	u_short	im6o_max_memberships;	/* max memberships this socket */
-	struct	in6_multi **im6o_membership;	/* group memberships */
-	struct	in6_mfilter *im6o_mfilters;	/* source filters */
-};
-
 struct route_in6 {
 	struct	rtentry *ro_rt;
 	struct	llentry *ro_lle;
