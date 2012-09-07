656e6ae629eeffb4f5d6155dcd01882292b2b467
t00fcxen <t00fcxen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2012-09-07 20:31:44 +0000

    More Windows stuff...

--- trunk/KERN/usrsctp/programs/discard_server.c
+++ trunk/KERN/usrsctp/programs/discard_server.c
@@ -32,6 +32,9 @@
  * Usage: discard_server [local_encaps_port] [remote_encaps_port]
  */
 
+#ifdef _WIN32
+#define _CRT_SECURE_NO_WARNINGS
+#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
