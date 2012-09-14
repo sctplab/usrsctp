950ba914d8f335c6df9e0b03dd634a25b6e4bf5c
t00fcxen <t00fcxen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2012-09-14 18:34:06 +0000

    Sync with main sources.

--- trunk/KERN/usrsctp/usrsctplib/netinet/sctputil.c
+++ trunk/KERN/usrsctp/usrsctplib/netinet/sctputil.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctputil.c 240148 2012-09-05 18:52:01Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctputil.c 240507 2012-09-14 18:32:20Z tuexen $");
 #endif
 
 #include <netinet/sctp_os.h>
