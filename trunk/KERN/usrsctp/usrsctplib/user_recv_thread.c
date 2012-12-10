1643c861b48da42f90258c5b8e6959d74bb5a011
t00fcxen <t00fcxen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2012-12-10 20:28:19 +0000

    Use consistently new defines.

--- trunk/KERN/usrsctp/usrsctplib/user_recv_thread.c
+++ trunk/KERN/usrsctp/usrsctplib/user_recv_thread.c
@@ -315,7 +315,7 @@ recv_function_raw(void *arg)
 			   as usual without having the packet header.
 			   Have tried both sending and receiving
 			 */
-			recvmbuf[i] = sctp_get_mbuf_for_msg(iovlen, want_header, M_DONTWAIT, want_ext, MT_DATA);
+			recvmbuf[i] = sctp_get_mbuf_for_msg(iovlen, want_header, M_NOWAIT, want_ext, MT_DATA);
 #if !defined(__Userspace_os_Windows)
 			recv_iovec[i].iov_base = (caddr_t)recvmbuf[i]->m_data;
 			recv_iovec[i].iov_len = iovlen;
@@ -492,7 +492,7 @@ recv_function_raw6(void *arg)
 			   as usual without having the packet header.
 			   Have tried both sending and receiving
 			 */
-			recvmbuf6[i] = sctp_get_mbuf_for_msg(iovlen, want_header, M_DONTWAIT, want_ext, MT_DATA);
+			recvmbuf6[i] = sctp_get_mbuf_for_msg(iovlen, want_header, M_NOWAIT, want_ext, MT_DATA);
 #if !defined(__Userspace_os_Windows)
 			recv_iovec[i].iov_base = (caddr_t)recvmbuf6[i]->m_data;
 			recv_iovec[i].iov_len = iovlen;
@@ -675,7 +675,7 @@ recv_function_udp(void *arg)
 			   as usual without having the packet header.
 			   Have tried both sending and receiving
 			 */
-			udprecvmbuf[i] = sctp_get_mbuf_for_msg(iovlen, want_header, M_DONTWAIT, want_ext, MT_DATA);
+			udprecvmbuf[i] = sctp_get_mbuf_for_msg(iovlen, want_header, M_NOWAIT, want_ext, MT_DATA);
 #if !defined(__Userspace_os_Windows)
 			iov[i].iov_base = (caddr_t)udprecvmbuf[i]->m_data;
 			iov[i].iov_len = iovlen;
@@ -874,7 +874,7 @@ recv_function_udp6(void *arg)
 			   as usual without having the packet header.
 			   Have tried both sending and receiving
 			 */
-			udprecvmbuf6[i] = sctp_get_mbuf_for_msg(iovlen, want_header, M_DONTWAIT, want_ext, MT_DATA);
+			udprecvmbuf6[i] = sctp_get_mbuf_for_msg(iovlen, want_header, M_NOWAIT, want_ext, MT_DATA);
 #if !defined(__Userspace_os_Windows)
 			iov[i].iov_base = (caddr_t)udprecvmbuf6[i]->m_data;
 			iov[i].iov_len = iovlen;
