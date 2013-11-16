e4a78083a6690fa3dfa329bf076c4b3748b0b645
t00fcxen <t00fcxen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2013-11-16 15:08:20 +0000

    Sync FBSDIDs.

--- usrsctplib/netinet/sctp_pcb.c
+++ usrsctplib/netinet/sctp_pcb.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_pcb.c 257574 2013-11-03 14:00:17Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_pcb.c 258221 2013-11-16 15:04:49Z tuexen $");
 #endif
 
 #include <netinet/sctp_os.h>
