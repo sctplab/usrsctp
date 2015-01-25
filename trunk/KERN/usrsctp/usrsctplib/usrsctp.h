39de3ad924c82dc16e7e2556ebeaed8fbe295d1c
t00fcxen <t00fcxen@9df1edf5-d72c-5b5f-11c0-5f5209eb73f7>
2015-01-25 12:14:32 +0000

    Get the header compiling when used by D as reported by Joakim.

--- trunk/KERN/usrsctp/usrsctplib/usrsctp.h
+++ trunk/KERN/usrsctp/usrsctplib/usrsctp.h
@@ -827,6 +827,8 @@ struct sctp_timeouts {
 
 /******************** System calls *************/
 
+struct socket;
+
 void
 usrsctp_init(uint16_t,
              int (*)(void *addr, void *buffer, size_t length, uint8_t tos, uint8_t set_df),
