86d0375b6582df1f81fe8dce305cd57a2853e1b9
Michael Tuexen <tuexen@fh-muenster.de>
2015-10-13 20:30:47 +0200

    Fix the timeout for INIT chunks when RTO_MIN < RTO_INITIAL.

--- usrsctplib/netinet/sctp_timer.c
+++ usrsctplib/netinet/sctp_timer.c
@@ -32,7 +32,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_timer.c 286781 2015-08-14 14:26:13Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_timer.c 289240 2015-10-13 18:27:55Z tuexen $");
 #endif
 
 #define _IP_VHL
@@ -403,7 +403,11 @@ sctp_backoff_on_timeout(struct sctp_tcb *stcb,
     int num_marked, int num_abandoned)
 {
 	if (net->RTO == 0) {
-		net->RTO = stcb->asoc.minrto;
+		if (net->RTO_measured) {
+			net->RTO = stcb->asoc.minrto;
+		} else {
+			net->RTO = stcb->asoc.initial_rto;
+		}
 	}
 	net->RTO <<= 1;
 	if (net->RTO > stcb->asoc.maxrto) {
