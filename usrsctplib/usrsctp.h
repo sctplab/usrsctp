23dc752d65e753de51a3c4e6ef0145a6b02a789c
Michael Tuexen <tuexen@fh-muenster.de>
2016-05-10 12:29:23 +0200

    Define uint64_t.
    Thanks to Radek Malcic for reporting the issue and providing a
    fix. This was detected on MINGW.

--- usrsctplib/usrsctp.h
+++ usrsctplib/usrsctp.h
@@ -67,6 +67,7 @@ extern "C" {
 #define uint8_t   unsigned __int8
 #define uint16_t  unsigned __int16
 #define uint32_t  unsigned __int32
+#define uint64_t  unsigned __int64
 #define int16_t   __int16
 #define int32_t   __int32
 #endif
