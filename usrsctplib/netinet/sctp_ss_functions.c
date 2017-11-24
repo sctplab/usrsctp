0e076261b832121cf120ddc04aaff87ac3a34d30
Michael Tuexen <tuexen@fh-muenster.de>
2017-11-24 20:45:25 +0100

    Fix SPDX line.

--- usrsctplib/netinet/sctp_ss_functions.c
+++ usrsctplib/netinet/sctp_ss_functions.c
@@ -1,5 +1,5 @@
 /*-
- * SPDX-License-Identifier: BSD-2-Clause
+ * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
  *
  * Copyright (c) 2010-2012, by Michael Tuexen. All rights reserved.
  * Copyright (c) 2010-2012, by Randall Stewart. All rights reserved.
@@ -30,7 +30,7 @@
 
 #ifdef __FreeBSD__
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: head/sys/netinet/sctp_ss_functions.c 326154 2017-11-24 11:25:53Z tuexen $");
+__FBSDID("$FreeBSD: head/sys/netinet/sctp_ss_functions.c 326180 2017-11-24 19:38:59Z tuexen $");
 #endif
 
 #include <netinet/sctp_pcb.h>
