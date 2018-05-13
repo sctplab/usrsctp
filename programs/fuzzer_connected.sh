0c461d2fffb225804865dcf6e026c2fd2af1c989
Felix Weinrank <weinrank@fh-muenster.de>
2018-05-13 21:01:58 +0200

    improve testing scripts

--- programs/fuzzer_connected.sh
+++ programs/fuzzer_connected.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-ulimit -c unlimited
 export ASAN_OPTIONS=abort_on_error=1:disable_core=0:unmap_shadow_on_exit=1:disable_coredump=0
-./fuzzer_connected -jobs=32 -timeout=10 -max_len=4086 CORPUS
+ulimit -c unlimited
+mkdir -p CORPUS_CONNECTED
+./fuzzer_connected -jobs=32 -timeout=10 -max_len=4086 CORPUS_CONNECTED
--- programs/fuzzer_unconnected.sh
+++ programs/fuzzer_unconnected.sh
@@ -1,2 +1,5 @@
-#!/bin/sh
-./fuzzer_unconnected -jobs=32 -timeout=10 -max_len=4086 CORPUS2
+#!/bin/bash
+export ASAN_OPTIONS=abort_on_error=1:disable_core=0:unmap_shadow_on_exit=1:disable_coredump=0
+ulimit -c unlimited
+mkdir -p CORPUS_UNCONNECTED
+./fuzzer_unconnected -jobs=32 -timeout=10 -max_len=4086 CORPUS_UNCONNECTED
