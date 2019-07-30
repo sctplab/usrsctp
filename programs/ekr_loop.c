7597c480419f9cfb2bb1a06196319a39c963a1b8
Felix Weinrank <weinrank@fh-muenster.de>
2019-07-30 23:45:46 +0200

    revert changes

--- programs/ekr_loop.c
+++ programs/ekr_loop.c
@@ -487,8 +487,7 @@ main(int argc, char *argv[])
 	sndinfo.snd_ppid = htonl(DISCARD_PPID);
 	sndinfo.snd_context = 0;
 	sndinfo.snd_assoc_id = 0;
-	i = 0;
-	while (i < NUMBER_OF_STEPS) {
+	for (i = 1; i < NUMBER_OF_STEPS; i++) {
 		if (i % 2) {
 			sndinfo.snd_flags = SCTP_UNORDERED;
 		} else {
@@ -497,38 +496,15 @@ main(int argc, char *argv[])
 		/* Send a 1 MB ordered message */
 		if (usrsctp_sendv(s_c, line, LINE_LENGTH, NULL, 0, (void *)&sndinfo,
 				 (socklen_t)sizeof(struct sctp_sndinfo), SCTP_SENDV_SNDINFO, 0) < 0) {
-			if (errno == EWOULDBLOCK) {
-				fprintf(stderr, "usrsctp_sendv() failed with EWOULDBLOCK - retry\n");
-#ifdef _WIN32
-				Sleep(1000);
-#else
-				sleep(1);
-#endif
-				continue;
-			} else {
-				perror("usrsctp_sendv");
-				fprintf(stderr, "usrsctp_sendv() failed - errno : %d\n", errno);
-				exit(EXIT_FAILURE);
-			}
+			perror("usrsctp_sendv");
+			exit(EXIT_FAILURE);
 		}
 		/* Send a 1 MB ordered message */
 		if (usrsctp_sendv(s_c, line, LINE_LENGTH, NULL, 0, (void *)&sndinfo,
 				 (socklen_t)sizeof(struct sctp_sndinfo), SCTP_SENDV_SNDINFO, 0) < 0) {
-			if (errno == EWOULDBLOCK) {
-				fprintf(stderr, "usrsctp_sendv() failed with EWOULDBLOCK - retry\n");
-#ifdef _WIN32
-				Sleep(1000);
-#else
-				sleep(1);
-#endif
-				continue;
-			} else {
-				perror("usrsctp_sendv");
-				fprintf(stderr, "usrsctp_sendv() failed - errno : %d\n", errno);
-				exit(EXIT_FAILURE);
-			}
+			perror("usrsctp_sendv");
+			exit(EXIT_FAILURE);
 		}
-		i++;
 #ifdef _WIN32
 		Sleep(1000);
 #else
