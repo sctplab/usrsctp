d32e4feb78ef085299194519c5606ca1b95a37c7
Felix Weinrank <weinrank@fh-muenster.de>
2019-08-22 15:20:07 +0200

    sync

--- fuzzer/fuzzer_connected_upcall.sh
+++ fuzzer/fuzzer_connected_upcall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export ASAN_OPTIONS=abort_on_error=1:disable_core=0:unmap_shadow_on_exit=1:disable_coredump=0
 ulimit -c unlimited
-mkdir -p CORPUS_UPCALL
+mkdir -p CORPUS_CONNECTED
 ./fuzzer_connected_upcall -jobs=32 -timeout=10 -max_len=4086 CORPUS_CONNECTED
