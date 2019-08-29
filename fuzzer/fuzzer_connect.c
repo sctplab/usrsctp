252756ca87d706449def5ffb8e0c4a77dd6b6bda
Felix Weinrank <weinrank@fh-muenster.de>
2019-08-30 00:45:32 +0200

    sync

--- fuzzer/fuzzer_connect.c
+++ fuzzer/fuzzer_connect.c
@@ -306,7 +306,7 @@ LLVMFuzzerTestOneInput(const uint8_t* data, size_t data_size)
 	usrsctp_conninput((void *)1, pktbuf, data_size + 12, 0);
 
 	usrsctp_close(socket_client);
-	//free(pktbuf);
+	free(pktbuf);
 	return (0);
 }
 
