8c40f3b5443deb76b2461615fd31c400fb61141a
Felix Weinrank <weinrank@fh-muenster.de>
2019-08-30 00:33:57 +0200

    sync

--- fuzzer/chain.sh
+++ fuzzer/chain.sh
@@ -5,7 +5,7 @@
 make
 echo "starte"
 #./fuzzer_connected CORPUS_CONNECTED/tsctp-000005 2>fuzzer.log
-./fuzzer_connected 2>fuzzer.log
+./fuzzer_connect_established CORPUS_CONNECT/data-1.bin 2>fuzzer.log
 echo "fertig"
 text2pcap -n -l 248 -D -t "%H:%M:%S." fuzzer.log fuzzer.pcapng
 wireshark fuzzer.pcapng
--- fuzzer/fuzzer_connect_cookie_wait.sh
+++ fuzzer/fuzzer_connect_cookie_wait.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export ASAN_OPTIONS=abort_on_error=1:disable_core=0:unmap_shadow_on_exit=1:disable_coredump=0
 ulimit -c unlimited
-mkdir -p CORPUS_CONNECT_COOKIE_WAIT
-./fuzzer_connect_cookie_wait -jobs=32 -timeout=10 -max_len=4086 CORPUS_CONNECT_COOKIE_WAIT
+mkdir -p CORPUS_CONNECT
+./fuzzer_connect_cookie_wait -jobs=32 -timeout=10 -max_len=4086 CORPUS_CONNECT
