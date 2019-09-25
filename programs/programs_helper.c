d276298ee5bc1a0fb19c2821d9e8689b35885729
Felix Weinrank <weinrank@fh-muenster.de>
2019-09-25 08:46:42 +0200

    WIP

--- programs/programs_helper.c
+++ programs/programs_helper.c
@@ -32,11 +32,8 @@ gettimeofday(struct timeval *tv, void *ignore)
 #endif
 
 void
-debug_printf(const char *format, ...)
-{
+debug_printf_runtime(void) {
 	static struct timeval time_main;
-
-	va_list ap;
 	struct timeval time_now;
 	struct timeval time_delta;
 
@@ -48,10 +45,6 @@ debug_printf(const char *format, ...)
 	timersub(&time_now, &time_main, &time_delta);
 
 	fprintf(stderr, "[%u.%03u] ", (unsigned int) time_delta.tv_sec, (unsigned int) time_delta.tv_usec / 1000);
-
-	va_start(ap, format);
-	vprintf(format, ap);
-	va_end(ap);
 }
 
 static void
