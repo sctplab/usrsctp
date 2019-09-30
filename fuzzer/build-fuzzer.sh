6b872d3ae2dcd4aa0779bfb8dce237dbb9828078
Felix Weinrank <weinrank@fh-muenster.de>
2019-09-30 14:53:46 +0200

    fix buildscript

--- fuzzer/build-fuzzer.sh
+++ fuzzer/build-fuzzer.sh
@@ -26,11 +26,15 @@ echo "OS :" $(uname)
 echo "CC :" $CC
 echo "NP :" $NPROC
 
+cd ..
+
 # Find and then delete all files under current directory (.) that:
 #  1. contains "cmake" (case-&insensitive) in its path (wholename)
 #  2. name is not CMakeLists.txt
 find . -iwholename '*cmake*' -not -name CMakeLists.txt -delete
 
+# Build with ASAN / MSAN
 cmake -Dsctp_build_fuzzer=1 -Dsctp_build_programs=0 -Dsctp_invariants=1 -Dsctp_sanitizer_address=1  -DCMAKE_C_COMPILER="$CC" -DCMAKE_BUILD_TYPE=RelWithDebInfo .
 #cmake -Dsctp_build_fuzzer=1 -Dsctp_build_programs=0 -Dsctp_invariants=1 -Dsctp_sanitizer_memory=1  -DCMAKE_C_COMPILER="$CC" -DCMAKE_BUILD_TYPE=RelWithDebInfo .
+
 make -j"$NPROC"
