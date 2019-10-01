468bf9e1374f98731335f94963f70bebf8be467f
Yura Yaroshevich <yura.yaroshevich@gmail.com>
2019-10-01 22:04:28 +0300

    Explicit constant.

--- usrsctplib/netinet/sctp_callout.c
+++ usrsctplib/netinet/sctp_callout.c
@@ -230,7 +230,7 @@ sctp_handle_tick(uint32_t elapsed_ticks)
 	ticks += elapsed_ticks;
 	c = TAILQ_FIRST(&SCTP_BASE_INFO(callqueue));
 	while (c) {
-		if ((c->c_time - ticks) & (1UL << 31)) {
+		if ((ticks - c->c_time) & 0x80000000) {
 			sctp_os_timer_next = TAILQ_NEXT(c, tqe);
 			TAILQ_REMOVE(&SCTP_BASE_INFO(callqueue), c, tqe);
 			c_func = c->c_func;
