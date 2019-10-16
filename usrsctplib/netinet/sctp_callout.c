c028acf640059988e8ad7785d44ea3d525d72bf5
Yura Yaroshevich <yura.yaroshevich@gmail.com>
2019-10-16 18:13:22 +0300

    Fix for "ISO C forbids conversion of function pointer to object pointer type"

--- usrsctplib/netinet/sctp_callout.c
+++ usrsctplib/netinet/sctp_callout.c
@@ -163,8 +163,7 @@ void
 sctp_os_timer_describe(const sctp_os_timer_t* t,
                        size_t max_len,
                        char* buffer) {
-	snprintf(buffer, max_len, "t=%" PRIu32 ",f=%p,a=%p", t->c_time,
-		(void*)t->c_func, (void*)t->c_arg);
+	snprintf(buffer, max_len, "t=%" PRIu32 ",a=%p", t->c_time, t);
 }
 
 int
