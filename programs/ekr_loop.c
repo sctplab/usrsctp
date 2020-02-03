d41cd176d384551893e3f4fd4ce037cbaec97e8a
Felix Weinrank <weinrank@fh-muenster.de>
2020-02-03 08:03:04 +0100

    sync

--- programs/ekr_loop.c
+++ programs/ekr_loop.c
@@ -274,7 +274,7 @@ main(int argc, char *argv[])
 #else
 	pthread_t tid_c, tid_s;
 #endif
-	int i, j, cur_buf_size, snd_buf_size, rcv_buf_size, retry_counter;
+	int i, j, cur_buf_size, snd_buf_size, rcv_buf_size, sendv_retries_left;
 	socklen_t opt_len;
 	struct sctp_sndinfo sndinfo;
 	char *line;
@@ -491,7 +491,7 @@ main(int argc, char *argv[])
 	sndinfo.snd_context = 0;
 	sndinfo.snd_assoc_id = 0;
 
-	retry_counter = 0;
+
 
 	for (i = 0; i < NUMBER_OF_STEPS; i++) {
 		j = 0;
@@ -501,14 +501,15 @@ main(int argc, char *argv[])
 			sndinfo.snd_flags = 0;
 		}
 		/* Send a 1 MB message */
+		sendv_retries_left = 120;
 		debug_printf("usrscp_sendv - step %d - call %d flags %x\n", i, ++j, sndinfo.snd_flags);
 		while (usrsctp_sendv(s_c, line, LINE_LENGTH, NULL, 0, (void *)&sndinfo,
 				 (socklen_t)sizeof(struct sctp_sndinfo), SCTP_SENDV_SNDINFO, 0) < 0) {
 			fprintf(stderr,"usrsctp_sendv - errno: %d - %s\n", errno, strerror(errno));
-			if (errno != EWOULDBLOCK || retry_counter > 240) {
+			if (errno != EWOULDBLOCK || !sendv_retries_left) {
 				exit(EXIT_FAILURE);
 			}
-			retry_counter++;
+			sendv_retries_left--;
 #ifdef _WIN32
 			Sleep(1000);
 #else
@@ -516,13 +517,14 @@ main(int argc, char *argv[])
 #endif
 		}
 		/* Send a 1 MB message */
+		sendv_retries_left = 120;
 		debug_printf("usrscp_sendv - step %d - call %d flags %x\n", i, ++j, sndinfo.snd_flags);
 		while (usrsctp_sendv(s_c, line, LINE_LENGTH, NULL, 0, (void *)&sndinfo,
 				 (socklen_t)sizeof(struct sctp_sndinfo), SCTP_SENDV_SNDINFO, 0) < 0) {
-			if (errno != EWOULDBLOCK || retry_counter > 240) {
+			if (errno != EWOULDBLOCK || !sendv_retries_left) {
 				fprintf(stderr,"usrsctp_sendv - errno: %d - %s\n", errno, strerror(errno));
 			}
-			retry_counter++;
+			sendv_retries_left--;
 #ifdef _WIN32
 			Sleep(1000);
 #else
