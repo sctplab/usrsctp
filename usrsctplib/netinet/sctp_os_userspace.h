42d4c44ed823fc2ba709f03bab27fa449c1f5f36
Ole André Vadla Ravnås <oleavr@gmail.com>
2022-03-28 19:49:47 +0000

    Fix QNX support
    - Revive QNX parts of sctp_os_userspace.h, removed in 13a3b53.
    - Fix the sockaddr_conn definition.

--- usrsctplib/netinet/sctp_os_userspace.h
+++ usrsctplib/netinet/sctp_os_userspace.h
@@ -1076,6 +1076,8 @@ sctp_get_mbuf_for_msg(unsigned int space_needed, int want_header, int how, int a
 #define CMSG_ALIGN(n)   _ALIGN(n)
 #elif defined(__NetBSD__)
 #define CMSG_ALIGN(n)   (((n) + __ALIGNBYTES) & ~__ALIGNBYTES)
+#elif defined(__QNX__)
+#define CMSG_ALIGN(n)   __CMSG_ALIGN(n)
 #elif defined(__APPLE__)
 #if !defined(__DARWIN_ALIGNBYTES)
 #define	__DARWIN_ALIGNBYTES	(sizeof(__darwin_size_t) - 1)
@@ -1139,7 +1141,7 @@ sctp_get_mbuf_for_msg(unsigned int space_needed, int want_header, int how, int a
 
 #define SCTP_IS_LISTENING(inp) ((inp->sctp_flags & SCTP_PCB_FLAGS_ACCEPTING) != 0)
 
-#if defined(__APPLE__) || defined(__DragonFly__) || defined(__linux__) || defined(__native_client__) || defined(__NetBSD__) || defined(_WIN32) || defined(__Fuchsia__) || defined(__EMSCRIPTEN__)
+#if defined(__APPLE__) || defined(__DragonFly__) || defined(__linux__) || defined(__native_client__) || defined(__NetBSD__) || defined(__QNX__) || defined(_WIN32) || defined(__Fuchsia__) || defined(__EMSCRIPTEN__)
 int
 timingsafe_bcmp(const void *, const void *, size_t);
 #endif
