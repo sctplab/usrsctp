b05d28bb413dc8353f82ae6f934d80d9fbfba045
Ole André Vadla Ravnås <oleavr@gmail.com>
2022-04-06 12:18:54 +0000

    Simplify init_random() to remove QNX special-case
    As it has both device entries and they provide the same functionality.

--- usrsctplib/user_environment.c
+++ usrsctplib/user_environment.c
@@ -175,18 +175,12 @@ finish_random(void)
 #elif (defined(__ANDROID__) && (__ANDROID_API__ < 28)) || defined(__QNX__) || defined(__EMSCRIPTEN__)
 #include <fcntl.h>
 
-#ifdef __QNX__
-#define SCTP_RANDOM_DEVICE "/dev/random"
-#else
-#define SCTP_RANDOM_DEVICE "/dev/urandom"
-#endif
-
 static int fd = -1;
 
 void
 init_random(void)
 {
-	fd = open(SCTP_RANDOM_DEVICE, O_RDONLY);
+	fd = open("/dev/urandom", O_RDONLY);
 	return;
 }
 
