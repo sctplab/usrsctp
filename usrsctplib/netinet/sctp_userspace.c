b4883fcadfe87d5e6b4756baf0a7116e6b950fca
Axel Rietschin <axelriet@gmail.com>
2024-01-12 12:38:25 -0800

    Make sctp_userspace_set_threadname() work on Windows.

--- usrsctplib/netinet/sctp_userspace.c
+++ usrsctplib/netinet/sctp_userspace.c
@@ -80,17 +80,60 @@ sctp_userspace_thread_create(userland_thread_t *thread, start_routine_t start_ro
 }
 #endif
 
+#if defined(_MSC_VER)
+
+//
+// https://learn.microsoft.com/en-us/visualstudio/debugger/how-to-set-a-thread-name-in-native-code?view=vs-2022
+//
+
+const DWORD MS_VC_EXCEPTION = 0x406D1388;
+
+#pragma pack(push, 8)
+typedef struct tagTHREADNAME_INFO
+{
+    DWORD dwType;     // Must be 0x1000.
+    LPCSTR szName;    // Pointer to name (in user addr space).
+    DWORD dwThreadID; // Thread ID (-1=caller thread).
+    DWORD dwFlags;    // Reserved for future use, must be zero.
+} THREADNAME_INFO;
+#pragma pack(pop)
+
+void SetThreadName (DWORD dwThreadID, const char *threadName)
+{
+    THREADNAME_INFO info;
+    info.dwType = 0x1000;
+    info.szName = threadName;
+    info.dwThreadID = dwThreadID;
+    info.dwFlags = 0;
+
+#pragma warning(push)
+#pragma warning(disable : 6320 6322)
+    __try {
+        RaiseException (MS_VC_EXCEPTION, 0, sizeof (info) / sizeof (ULONG_PTR),
+                        (ULONG_PTR *) &info);
+    }
+    __except (EXCEPTION_EXECUTE_HANDLER) {
+    }
+#pragma warning(pop)
+}
+
+#endif
+
 void
 sctp_userspace_set_threadname(const char *name)
 {
+    const char *n = name ? name : "";
+
 #if defined(__APPLE__)
-	pthread_setname_np(name);
-#endif
-#if defined(__linux__)
-	prctl(PR_SET_NAME, name);
-#endif
-#if defined(__FreeBSD__)
-	pthread_set_name_np(pthread_self(), name);
+	pthread_setname_np(n);
+#elif defined(__linux__)
+	prctl(PR_SET_NAME, n);
+#elif defined(__FreeBSD__)
+	pthread_set_name_np(pthread_self(), n);
+#elif defined(_MSC_VER)
+    if (IsDebuggerPresent ()) {
+        SetThreadName(GetCurrentThreadId(), n);
+    }
 #endif
 }
 
