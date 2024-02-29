e719a630b069fbd7e823007d0c9aa113ec006f89
Jonathan Lennox <jonathan.lennox@8x8.com>
2024-02-29 09:46:06 -0500

    Assert if soref() is called on an already-released socket.

--- usrsctplib/user_socketvar.h
+++ usrsctplib/user_socketvar.h
@@ -403,6 +403,7 @@ void	sofree(struct socket *so);
 
 #define	soref(so) do {							\
 	SOCK_LOCK_ASSERT(so);						\
+	KASSERT((so)->so_count => 0, ("soref"));			\
 	++(so)->so_count;						\
 } while (0)
 
@@ -410,8 +411,10 @@ void	sofree(struct socket *so);
 	ACCEPT_LOCK_ASSERT();						\
 	SOCK_LOCK_ASSERT(so);						\
 	KASSERT((so)->so_count > 0, ("sorele"));			\
-	if (--(so)->so_count == 0)					\
+	if (--(so)->so_count == 0) {					\
+		(so)->so_count = -1;					\
 		sofree(so);						\
+	}								\
 	else {								\
 		SOCK_UNLOCK(so);					\
 		ACCEPT_UNLOCK();					\
